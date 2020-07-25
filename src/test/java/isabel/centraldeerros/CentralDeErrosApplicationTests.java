9ebe9826787f9a8c9d57c3038463664abbf4c88f
isabellima <bellima93@gmail.com>
2020-07-25 12:00:55 -0300

    First commit

---
+++ src/test/java/isabel/centraldeerros/CentralDeErrosApplicationTests.java
@@ -0,0 +1,13 @@
+package isabel.centraldeerros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentralDeErrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
