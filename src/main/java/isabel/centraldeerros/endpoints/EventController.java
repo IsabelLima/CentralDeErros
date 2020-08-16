7317492b570099df472c323cba3cbb4ef6c1dd0e
Isabel Lima <bellima93@gmail.com>
2020-08-16 17:40:35 -0300

    Adding path params to controller

--- src/main/java/isabel/centraldeerros/endpoints/EventController.java
+++ src/main/java/isabel/centraldeerros/endpoints/EventController.java
@@ -4,12 +4,19 @@ import isabel.centraldeerros.dto.EventDTO;
 import isabel.centraldeerros.entity.Event;
 import isabel.centraldeerros.mappers.EventMapper;
 import isabel.centraldeerros.service.impl.EventService;
+import org.apache.tomcat.jni.Local;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.query.Param;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
+
+import javax.websocket.server.PathParam;
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 
 @RestController
@@ -33,7 +40,15 @@ public class EventController {
     }
 
     @GetMapping
-    public Iterable<EventDTO> findAll(Pageable pageable) {
+    public Iterable<EventDTO> findAll(@PathParam("level") Optional<String> level,
+                                      @PathParam("description") Optional<String> description,
+                                      @PathParam("log") Optional<String> log,
+                                      @PathParam("source")  Optional <String> source,
+                                      @PathParam("date")
+                                          @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) Optional<LocalDateTime> date,
+                                      @PathParam("quantity") Optional<Integer> quantity,
+                                      Pageable pageable) {
+
         return  mapper.map(this.eventService.findAll(pageable));
     }
 }
--- src/main/java/isabel/centraldeerros/mappers/EventMapper.java
+++ src/main/java/isabel/centraldeerros/mappers/EventMapper.java
@@ -11,7 +11,6 @@ import java.util.List;
 @Mapper(componentModel = "spring")
 public interface EventMapper {
 
-    //@Mapping(target = "log", ignore = true)
     EventDTO map(Event event);
     List<EventDTO> map(List<Event> events);
 }
